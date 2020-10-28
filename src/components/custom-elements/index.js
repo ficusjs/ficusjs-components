fcc3fbed8c2afd2f0ad75f932d2aee2c79c04f0d
Matt Levy <ducksoupdev@gmail.com>
2020-10-28 11:34:46 +0000

    feat: add separate html and custom elements

--- src/components/custom-elements/index.js
+++ src/components/custom-elements/index.js
@@ -2,7 +2,7 @@ export const components = {
   button: () => import('./button/index.js')
 }
 
-export function loadComponents(baseComponents = []) {
+export function loadComponents (baseComponents = []) {
   if (!baseComponents.length) return Promise.reject(new Error('No base components specified'))
   const componentPromises = baseComponents.map(a => components[a]())
   return Promise.all(componentPromises)
