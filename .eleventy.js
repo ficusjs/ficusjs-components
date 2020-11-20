c07d4ba9f8c9a0dcbe1cb2271f983e4a86fc4ba4
James Cavanagh <james.cavanagh@oneadvanced.com>
2020-11-20 17:54:43 +0000

    feat: separate site and component assets/styles

--- .eleventy.js
+++ .eleventy.js
@@ -44,9 +44,7 @@ module.exports = function (eleventyConfig) {
     }
   })
 
-  eleventyConfig.addPassthroughCopy([
-    "src/img"
-  ])
+  eleventyConfig.addPassthroughCopy("src/docs");
 
   eleventyConfig.addPlugin(syntaxHighlight)
 
