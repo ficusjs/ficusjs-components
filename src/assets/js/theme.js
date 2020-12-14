6e8cf747a2d48f0e6f9fc6ce94442fe36e27b5a5
James Cavanagh <james.cavanagh@oneadvanced.com>
2020-12-14 21:20:58 +0000

    fix: dark theme toggle

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,13 +1,16 @@
 const currentTheme = localStorage.getItem('theme')
+const iFrameContent = document.getElementsByTagName("iframe")[0].contentWindow;
 
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme)
+  iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', currentTheme)
 }
 
 const darkToggleSwitch = document.getElementById('dark-theme-toggle')
 
 darkToggleSwitch.onclick = function switchTheme(e) {
   document.documentElement.setAttribute('data-theme', 'dark')
+  iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'dark')
   localStorage.setItem('theme', 'dark')
 }
 
@@ -15,5 +18,6 @@ darkToggleSwitch.onclick = function switchTheme(e) {
 const lightToggleSwitch = document.getElementById('light-theme-toggle')
 lightToggleSwitch.onclick = function switchTheme(e) {
   document.documentElement.setAttribute('data-theme', 'light')
+  iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'light')
   localStorage.setItem('theme', 'light')
 }
