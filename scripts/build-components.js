9d936cf9e45ae24d68f414b7d60cc00ef6d43d93
Matt Levy <ducksoupdev@gmail.com>
2020-12-28 14:22:06 +0000

    feat: alpha release 1.0.0-alpha.0

--- scripts/build-components.js
+++ scripts/build-components.js
@@ -1,10 +1,16 @@
 const glob = require('glob-all')
 const logSymbols = require('log-symbols')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const build = require('./lib/build')
+
+const argv = yargs(hideBin(process.argv)).argv
+
+const outputDir = argv.outputDir || 'build'
 
 const components = glob.sync(['src/components/custom-elements/*/index.js', 'src/components/loader'])
 const buildPromises = components.map(c => {
-  return build(c, c.replace('src', 'build'))
+  return build(c, c.replace('src', outputDir))
 })
 
 Promise.all(buildPromises)
