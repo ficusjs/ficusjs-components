758cb09028ac42eefeb1b3c4ac4f430de93b4ec0
James Cavanagh <james.cavanagh@oneadvanced.com>
2021-01-02 21:32:00 +0000

    feat: add svg logos

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -6,12 +6,18 @@ if (currentTheme) {
   iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', currentTheme)
 }
 
+
 const darkToggleSwitch = document.getElementById('dark-theme-toggle')
+
+const logo = document.getElementById('ficus-logo')
+const darkThemeLogo = document.getElementById('ficus-logo-white')
 
 darkToggleSwitch.onclick = function switchTheme(e) {
   document.documentElement.setAttribute('data-theme', 'dark')
   iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'dark')
   localStorage.setItem('theme', 'dark')
+  logo.style.display = 'none'
+  darkThemeLogo.style.display = 'block'
 }
 
 
@@ -20,4 +26,6 @@ lightToggleSwitch.onclick = function switchTheme(e) {
   document.documentElement.setAttribute('data-theme', 'light')
   iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'light')
   localStorage.setItem('theme', 'light')
+  logo.style.display = 'block'
+  darkThemeLogo.style.display = 'none'
 }
