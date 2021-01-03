fc06036791c328ccdcc195e21ea49779deef1d2b
James Cavanagh <james.cavanagh@oneadvanced.com>
2021-01-03 21:05:30 +0000

    feat: add theme toggle

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,31 +1,42 @@
-const currentTheme = localStorage.getItem('theme')
-const iFrameContent = document.getElementsByTagName("iframe")[0].contentWindow;
+// Theme Toggle
 
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme)
-  iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', currentTheme)
-}
-
-
-const darkToggleSwitch = document.getElementById('dark-theme-toggle')
-
+const toggleSwitch = document.querySelector('#theme-toggle')
+const iFrameContent = document.getElementsByTagName("iframe")[0].contentWindow
+const box = document.querySelector('.fd-theme-toggle__inner')
+const ball = document.querySelector('.fd-theme-toggle__ball')
 const logo = document.getElementById('ficus-logo')
 const darkThemeLogo = document.getElementById('ficus-logo-white')
+console.log(toggleSwitch)
+const currentTheme = localStorage.getItem('theme');
 
-darkToggleSwitch.onclick = function switchTheme(e) {
-  document.documentElement.setAttribute('data-theme', 'dark')
-  iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'dark')
-  localStorage.setItem('theme', 'dark')
-  logo.style.display = 'none'
-  darkThemeLogo.style.display = 'block'
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+
+    ball.setAttribute('style','transform:translatex(100%);')
+    logo.style.display = 'none'
+    darkThemeLogo.style.display = 'block'
+  }
 }
 
 
-const lightToggleSwitch = document.getElementById('light-theme-toggle')
-lightToggleSwitch.onclick = function switchTheme(e) {
-  document.documentElement.setAttribute('data-theme', 'light')
-  iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'light')
-  localStorage.setItem('theme', 'light')
-  logo.style.display = 'block'
-  darkThemeLogo.style.display = 'none'
-}
+
+toggleSwitch.addEventListener('change', e => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+    ball.setAttribute('style','transform:translatex(100%);')
+    logo.style.display = 'none'
+    darkThemeLogo.style.display = 'block'
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    iFrameContent.document.getElementsByTagName("html")[0].setAttribute('data-theme', 'light')
+    ball.setAttribute('style','transform:translatex(0%);')
+    logo.style.display = 'block'
+    darkThemeLogo.style.display = 'none'
+  }    
+})
