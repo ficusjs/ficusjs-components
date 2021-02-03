44d3edd0849e28e20bd0f6a5784667763f12d5ca
James Cavanagh <james.cavanagh@oneadvanced.com>
2021-02-03 13:01:33 +0000

    feat: add mobile navigation

--- src/assets/js/doc-theme.js
+++
@@ -1,5 +0,0 @@
-const currentTheme = window.localStorage.getItem('theme')
-
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme)
-}
--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,7 +1,6 @@
 // Theme Toggle
 
 const toggleSwitch = document.querySelector('#theme-toggle')
-// const iFrameContent = document.getElementsByTagName('iframe')[0].contentWindow
 const ball = document.querySelector('.fd-theme-toggle__ball')
 const logo = document.getElementById('ficus-logo')
 const darkThemeLogo = document.getElementById('ficus-logo-white')
@@ -21,7 +20,6 @@ if (currentTheme) {
 toggleSwitch.addEventListener('change', e => {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark')
-    // iFrameContent.document.getElementsByTagName('html')[0].setAttribute('data-theme', 'dark')
     window.localStorage.setItem('theme', 'dark')
     ball.setAttribute('style', 'transform:translatex(100%);')
     logo.style.display = 'none'
@@ -29,7 +27,6 @@ toggleSwitch.addEventListener('change', e => {
   } else {
     document.documentElement.setAttribute('data-theme', 'light')
     window.localStorage.setItem('theme', 'light')
-    // iFrameContent.document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light')
     ball.setAttribute('style', 'transform:translatex(0%);')
     logo.style.display = 'block'
     darkThemeLogo.style.display = 'none'
