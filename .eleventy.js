d85f637e679554155c6928b3b0715515aaa7f321
Matt Levy <ducksoupdev@gmail.com>
2021-03-09 10:11:10 +0000

    feat: remove local ficus docs styles

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,27 @@ const slugify = require("slugify")
 const pluginTOC = require('eleventy-plugin-toc')
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+const CleanCSS = require("clean-css")
+const htmlmin = require("html-minifier")
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
+      return minified
+    }
+    return content
+  })
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles
+  })
+
   eleventyConfig.addShortcode("componentPath", function (path) {
     return process.env.NODE_ENV === 'production' ? path : `http://localhost:8888${path}`
   })
