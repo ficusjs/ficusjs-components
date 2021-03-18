7e91da9ecac0b67bd8e7b5edf6911c178bac149f
Matt Levy <ducksoupdev@gmail.com>
2021-03-18 14:09:58 +0000

    feat: update site renderer to lit-html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,4 @@
 import { use } from 'https://cdn.skypack.dev/ficusjs'
-import { html, renderer } from 'https://cdn.skypack.dev/@ficusjs/renderers/uhtml'
+import { html, renderer } from 'https://cdn.skypack.dev/@ficusjs/renderers/lit-html'
 import { module as navbarModule } from 'https://cdn.skypack.dev/@ficusjs/components/custom-elements/navbar'
 use(navbarModule, { renderer, html })
